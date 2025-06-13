20f60caf9671794fecd6df65ec86b36cf4fb8502
bolaalcatras <jhonalexander2016.com@gmail.com>
2025-06-13 15:59:13 -0500

    prueba

--- run.py
+++ run.py
@@ -8,3 +8,4 @@ if __name__ == '__main__':
     # Usamos un solo puerto, por ejemplo el 5000
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)  
+     
